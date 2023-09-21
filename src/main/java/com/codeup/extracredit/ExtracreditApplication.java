a0caa29a27e279c2cf4fb2cf940edced29891e24
SamR0429 <Samanatha.ramos01@gmail.com>
2023-09-21 15:29:39 -0500

    added file

---
+++ src/main/java/com/codeup/extracredit/ExtracreditApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.extracredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtracreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtracreditApplication.class, args);
+	}
+
+}
