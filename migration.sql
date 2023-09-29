eb0b127b57c9fc84bc148de15e51ae932637aaa2
SamR0429 <Samanatha.ramos01@gmail.com>
2023-09-29 18:49:56 -0500

    added html files and worked on order controller

--- migration.sql
+++ migration.sql
@@ -1,5 +1,1 @@
-CREATE DATABASE spring_blog_extra_credit_db;
 
-CREATE DATABASE IF NOT EXISTS spring_blog_extra_credit_db;
-
-SHOW DATABASES;
--- src/main/java/com/codeup/extracredit/controllers/OrderController.java
+++ src/main/java/com/codeup/extracredit/controllers/OrderController.java
@@ -2,19 +2,32 @@ package com.codeup.extracredit.controllers;
 
 
 import com.codeup.extracredit.models.Order;
+import com.codeup.extracredit.repositories.OrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
 
-//    @GetMapping("/create")
-//    public String showCreateForm(Model model){
-//        model.addAttribute("order", new Order order);
-//    }
+    private OrderRepository orderDao;
+
+    public OrderController(OrderRepository orderDao){
+        this.orderDao = orderDao;
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model){
+        model.addAttribute("order", new Order());
+        return "orders/create";
+    }
+
+    @PostMapping("/create")
+    public String saveOrderForm(@ModelAttribute Order order){
+        orderDao.save(order);
+        //this line might be very long, but basically we need to save the given order and i dont think this was the right way to do it
+        return "redirect:/show";
+    }
 
 }
